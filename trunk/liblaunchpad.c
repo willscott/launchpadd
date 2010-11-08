0e5dfd38dd2611696ec5d3a3f3edba817723ec10
willscott@gmail.com <willscott@gmail.com@5f1faa6c-f355-11de-9907-777483041811>
2010-11-08 03:19:41 +0000

    Fixed a memory leak & polling timeout.  (Should actually work again!) A bit of respacing to make the code more readable.

--- trunk/liblaunchpad.c
+++ trunk/liblaunchpad.c
@@ -2,110 +2,113 @@
 
 void launchpad_read_callback(struct libusb_transfer *transfer)
 {
-    int i;
     struct launchpad_handle *dp = transfer->user_data;
     //figure out if we need to read something
-    if(transfer->actual_length > 0)
+    if (transfer->actual_length > 0)
     {
         dp->callback(transfer->buffer, transfer->actual_length, dp->user_data);
     }
-    else if(transfer->status != LIBUSB_TRANSFER_TIMED_OUT &&
-            transfer->status != LIBUSB_TRANSFER_COMPLETED)
+    else if (transfer->status != LIBUSB_TRANSFER_TIMED_OUT &&
+             transfer->status != LIBUSB_TRANSFER_COMPLETED)
     {
-        fprintf(stderr,"Reading failed with code %d\n",transfer->status);
+        fprintf(stderr, "Reading failed with code %d\n", transfer->status);
         return;
     }
     //resend it
     libusb_fill_interrupt_transfer(transfer,
-                                    dp->device,
-                                    USB_LP_INTR_IN,
-                                    transfer->buffer,
-                                    transfer->length,
-                                    launchpad_read_callback,
-                                    dp,
-                                    10);
-    if(libusb_submit_transfer(transfer) != 0)
+                                   dp->device,
+                                   USB_LP_INTR_IN,
+                                   transfer->buffer,
+                                   transfer->length,
+                                   launchpad_read_callback,
+                                   dp,
+                                   LP_POLL_INTERVAL);
+    if (libusb_submit_transfer(transfer) != 0)
     {
-        fprintf(stderr,"Couldn't send transfer\n");
-        return;       
+        fprintf(stderr, "Couldn't send transfer\n");
+        return;
     }
 }
+
 void launchpad_write_callback(struct libusb_transfer *transfer)
 {
     struct launchpad_handle *dp = transfer->user_data;
-    if(transfer->status != LIBUSB_TRANSFER_TIMED_OUT &&
-       transfer->status != LIBUSB_TRANSFER_COMPLETED)
+    if (transfer->status != LIBUSB_TRANSFER_TIMED_OUT &&
+        transfer->status != LIBUSB_TRANSFER_COMPLETED)
     {
-        fprintf(stderr,"Writing failed with code %d\n", transfer->status);
+        fprintf(stderr, "Writing failed with code %d\n", transfer->status);
+        fprintf(stderr, "and that is not %d or %d...\n", LIBUSB_TRANSFER_TIMED_OUT,LIBUSB_TRANSFER_COMPLETED);
     }
     dp->writing = 0;
     dp->callback(NULL, 0, dp->user_data);
 }
 
-struct launchpad_handle* launchpad_register(launchpad_callback e,void* user_data)
+struct launchpad_handle* launchpad_register(launchpad_callback e, void* user_data)
 {
     struct launchpad_handle *dp;
     unsigned int bufferlen;
     unsigned char* buffer;
-    
+
     //initialize usb
-    if(libusb_init(NULL)!=0)
+    if (libusb_init(NULL) != 0)
     {
-        fprintf(stderr,"Unable to initialize usb\n");
+        fprintf(stderr, "Unable to initialize usb\n");
         return NULL;
     }
-    
+
     //allocate the handle
     dp = malloc(sizeof(struct launchpad_handle));
-    if(dp == NULL)
+    if (dp == NULL)
     {
-        fprintf(stderr,"Unable to allocate handle\n");
+        fprintf(stderr, "Unable to allocate handle\n");
         return NULL;
     }
     dp->callback = e;
     dp->user_data = user_data;
-    
+
     //find the device
     dp->device = libusb_open_device_with_vid_pid(NULL, USB_LP_VENDOR_ID, USB_LP_PRODUCT_ID);
     if (dp->device == NULL) {
-        fprintf(stderr,"Unable to find the launchpad\n");
+        fprintf(stderr, "Unable to find the launchpad\n");
         return NULL;
     }
     //claim the device
-    if(libusb_claim_interface(dp->device, 0) != 0)
+    if (libusb_claim_interface(dp->device, 0) != 0)
     {
-        fprintf(stderr,"Unable to claim the launchpad\n");
+        fprintf(stderr, "Unable to claim the launchpad\n");
         return NULL;
     }
-    
+
     //create transfers
-    bufferlen = libusb_get_max_packet_size(libusb_get_device(dp->device),USB_LP_INTR_OUT);
+    bufferlen = libusb_get_max_packet_size(libusb_get_device(dp->device), USB_LP_INTR_OUT);
     dp->rtransfer = libusb_alloc_transfer(0);
     dp->wtransfer = libusb_alloc_transfer(0);
-    buffer = malloc(sizeof(char)*bufferlen);
-    if (dp->rtransfer == NULL || dp->wtransfer == NULL || buffer == NULL) {
-        if(bufferlen == LIBUSB_ERROR_NOT_FOUND) {
-            fprintf(stderr,"Couldn't Determine Packet Size\n");            
+    buffer = malloc(sizeof(char) * bufferlen);
+    if (dp->rtransfer == NULL || dp->wtransfer == NULL || buffer == NULL)
+    {
+        if (bufferlen == LIBUSB_ERROR_NOT_FOUND)
+        {
+            fprintf(stderr, "Couldn't Determine Packet Size\n");
         }
-        fprintf(stderr,"Unable to allocate transfers\n");
+        fprintf(stderr, "Unable to allocate transfers\n");
         return NULL;
     }
     //start transfers
     dp->writing = 0;
     libusb_fill_interrupt_transfer(dp->rtransfer,
-                                    dp->device,
-                                    USB_LP_INTR_IN,
-                                    buffer,
-                                    bufferlen,
-                                    launchpad_read_callback,
-                                    dp,
-                                    10);
-    if(libusb_submit_transfer(dp->rtransfer) != 0)
+                                   dp->device,
+                                   USB_LP_INTR_IN,
+                                   buffer,
+                                   bufferlen,
+                                   launchpad_read_callback,
+                                   dp,
+                                   LP_POLL_INTERVAL);
+    if (libusb_submit_transfer(dp->rtransfer) != 0)
     {
-        fprintf(stderr,"Unable to start reading\n");
-        return NULL;       
+        fprintf(stderr, "Unable to start reading\n");
+        return NULL;
     }
-        
+
     return dp;
 }
 
@@ -113,7 +116,9 @@ void launchpad_deregister(struct launchpad_handle* dp)
 {
     //cancel transfers
     if(dp->writing)
+    {
         libusb_cancel_transfer(dp->wtransfer);
+    }
     libusb_cancel_transfer(dp->rtransfer);
     //destroy transfers
     libusb_free_transfer(dp->rtransfer);
@@ -125,7 +130,7 @@ void launchpad_deregister(struct launchpad_handle* dp)
     //deallocate the handle
     free(dp);
     //close usb
-    libusb_exit(NULL);    
+    libusb_exit(NULL);
 }
 
 int launchpad_write(struct launchpad_handle *dp, unsigned char* data, size_t len)
@@ -157,13 +162,13 @@ int launchpad_poll(struct pollfd* descriptors, size_t num)
 {
     //lock events
     libusb_lock_events(NULL);
-    
+
     struct pollfd* final;
     struct timeval tv;
     int i = 0,usbevent = 0,yourevent = 0;
     nfds_t total = 0;
     const struct libusb_pollfd** lpfds = libusb_get_pollfds(NULL);
-    
+
     while(lpfds[i] != NULL) { i++; }
     total = num+i;
     final = malloc(sizeof(struct pollfd) * (i + num));
@@ -177,12 +182,13 @@ int launchpad_poll(struct pollfd* descriptors, size_t num)
         final[i+num].fd = lpfds[i]->fd;
         final[i+num].events = lpfds[i]->events;
     }
+    free(lpfds);
+//    tv.tv_sec = 0;
+//    tv.tv_usec = 0;
+//    libusb_get_next_timeout(NULL, &tv);
+//    poll(final, total, tv.tv_usec + 1000 * tv.tv_sec);
+    poll(final,total,120*1000);
 
-    tv.tv_sec = 0;
-    tv.tv_usec = 0;
-    libusb_get_next_timeout(NULL,&tv);
-    poll(final,total,tv.tv_usec+1000*tv.tv_sec);
- 
     for(i = 0; i < num; i++) {
         descriptors[i].revents = final[i].revents;
         if(final[i].revents != 0)
@@ -207,5 +213,6 @@ int launchpad_poll(struct pollfd* descriptors, size_t num)
     libusb_unlock_events(NULL);
 
     free(final);
+    final = NULL;
     return yourevent;
 }
